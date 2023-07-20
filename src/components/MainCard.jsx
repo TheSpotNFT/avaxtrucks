01a8443b6e5c828412183675e17bdd0f281d53f6
thejunglemarket <96501055+TheSpotNFT@users.noreply.github.com>
2023-07-19 22:08:10 -0700

    mobile

--- src/components/MainCard.jsx
+++ src/components/MainCard.jsx
@@ -88,10 +88,14 @@ function Card({
   };
 
   const [textinput, setTextinput] = useState("1");
+  const [textinput1, setTextinput1] = useState("1");
   const [totalRemaining, setTotalRemaining] = useState("1");
 
   const textinputUser = (event) => {
     setTextinput(event.target.value);
+  };
+  const textinputUser1 = (event) => {
+    setTextinput1(event.target.value);
   };
 
   let remaining;
@@ -184,7 +188,7 @@ function Card({
 
     <div className="items-center md:grid md:grid-cols-2 sm:grid-cols-1 hover:z-0 rounded shadow-lg bg-black bg-background bg-cover bg-opacity-90 opacity-90 duration-300 md:w-[600px] md:h-[500px] lg:h-auto lg:w-[800px] xl:w-[1050px] snap-start snap-mandatory z-0">
       <div>
-        <img className="w-full md:pt-8 md:pl-10 lg:pl-8 text-black p-2 m:p-8" src={hotpink} alt={alt}></img>
+        <img className="md:w-full sm:w-2/3 sm:mx-auto sm:pt-0 md:pl-10 lg:pl-8 text-black p-2" src={hotpink} alt={alt}></img>
         <div className="px-6 py-4">
 
 
@@ -219,21 +223,21 @@ function Card({
           </div>
         </div></div>
 
-      <div className={`pt-8 pr-8 pixelated text-white drop-shadow-2xl shadow-black w-full ${showDocs === true ? 'visible' : 'hidden'}`}>
+      <div className={`sm:pl-4 md:pl-0 md:pt-2 lg:pt-8 pr-4 pixelated text-white drop-shadow-2xl shadow-black w-full ${showDocs === true ? 'visible' : 'hidden'}`}>
         <div className='bg-black bg-opacity-60'>
-          <div className='pr-4 md:pt-8 lg-pt-16 lg:text-4xl md:text-4xl w-full pt-4 sm:text-2xl pb-4'>Avax Trucks</div>
-          <div className='lg:pt-2 xl:pt-20 sm:pt-4 md:text-3xl sm:text-lg w-full'>Supply: 10k</div>
-          <div className='pt-4 sm:text-lg md:text-3xl w-full'></div>
-          <div className='pt-4 sm:text-lg md:text-3xl w-full'>{line3}</div>
-          <div className='pt-4 sm:text-lg md:text-3xl w-full'>{line4}</div>
-          <div className='pt-4'><div className='pixelated pb-4'>Input your PDX Token ID: <input
+          <div className='md:pt-2 lg-pt-16 lg:text-4xl md:text-2xl w-full pt-4 sm:text-lg pb-4'>Avax Trucks</div>
+          <div className='md:pt-0 lg:pt-2 xl:pt-20 sm:pt-4 md:text-xl sm:text-lg w-full'>Supply: 10k</div>
+          <div className='md:pt-2 lg:pt-4 sm:text-lg md:text-xl w-full'></div>
+          <div className='md:pt-2 lg:pt-4 sm:text-lg md:text-xl w-full'>{line3}</div>
+          <div className='md:pt-2 lg:pt-4 sm:text-lg md:text-xl w-full'>{line4}</div>
+          <div className='pt-4'><div className='pixelated pb-4 px-4'>Input your PDX Token ID: <input
             type="number"
             className="border-2 border-slate-600 bg-slate-400 text-left pixelated text-lg placeholder-slate-600 pl-2 pr-4 w-16"
             placeholder="Amount"
-            value={textinput}
-            onChange={textinputUser.bind(this)}
-          /></div><button
-            className="m-1 w-1/2 rounded-sm pl-4 px-1 py-1 border-4 bg-black border-gray-200 text-white
+            value={textinput1}
+            onChange={textinputUser1.bind(this)}
+          /></div><div className='md:pr-2'><button
+            className="m-1 md:w-full lg:w-1/2 rounded-sm px-1 py-1 border-4 bg-black border-gray-200 text-white
      hover:bg-gray-200 hover:text-gray-900 duration-300 font-mansalva font-bold md:text-sm lg:text-md"
           /*disabled={props.txProcesssing}
         onClick={() => mintNFT()}
@@ -241,11 +245,11 @@ function Card({
             setShowDocs((v) => !v);
           }}*/
           >
-              Claim PDX Free Mints!
-            </button></div>
+            Claim PDX Free Mints!
+          </button></div></div>
           <div className='pt-4 md:pb-2 sm:text-lg md:text-lg lg:text-3xl w-full'></div></div>
         <div className='flex md:pt-0 lg:pt-0 xl:pt-4 pb-6 md:pb-6 sm:pt-4 sm:pb-2 text-lg w-full content-end p-4'><button
-          className="m-1 w-1/2 rounded-sm pl-4 px-1 py-1 border-4 bg-black border-gray-200 text-white
+          className="m-1 w-1/2 rounded-sm pr-2 pl-2 px-1 py-1 border-4 bg-black border-gray-200 text-white
      hover:bg-gray-200 hover:text-gray-900 duration-300 font-mansalva font-bold md:text-sm lg:text-lg"
           /*disabled={props.txProcesssing}
         onClick={() => mintNFT()}*/
@@ -255,8 +259,8 @@ function Card({
         >
           Info
         </button><button
-          className="m-1 w-1/2 rounded-sm px-1 py-1 border-4 bg-black border-gray-200 text-white
-     hover:bg-gray-200 hover:text-gray-900 duration-300 font-mansalva font-bold md:text-sm lg:text-sm"
+          className="m-1 sm:w-1/2 md:w-full lg:w-1/2 rounded-sm px-1 py-1 border-4 bg-black border-gray-200 text-white
+     hover:bg-gray-200 hover:text-gray-900 duration-300 font-mansalva font-bold md:text-xs lg:text-sm"
           /*disabled={props.txProcesssing}
         onClick={() => mintNFT()}*/
           onClick={onClickUrl("https://campfire.exchange/collections/0x20ef794f891c050d27bec63f50b202cce97d7224")}
@@ -266,7 +270,7 @@ function Card({
 
 
       </div>
-      <div className={`pr-8 pixelated text-white drop-shadow-2xl shadow-black w-full ${showDocs === true ? 'hidden' : 'visible'}`}>
+      <div className={`sm:pl-4 md:pl-0 md:pt-2 lg:pt-8 pr-4 pixelated text-white drop-shadow-2xl shadow-black w-full ${showDocs === true ? 'hidden' : 'visible'}`}>
 
         <div className='pixelated bg-black bg-opacity-60 text-white shadow-lg lg:pt-10 xl:pt-8 sm:pt-4 md:pt-10 xl:text-2xl lg:text-1xl md:text-base sm:text-lg w-full pb-10 px-4'>{docs}
           <div className='md:pt-10 sm:pt-4 md:text-2xl sm:text-md w-full'>Built for Truckers.</div>
